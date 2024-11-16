918d4eb1f3857e901e3b29993e79052dd0a925e2
OKAMOTO Shigehiro <primary@oka4shi.dev>
2024-11-16 12:58:48 +0900

    [scraper] format

--- scraper/hama-sushi.py
+++ scraper/hama-sushi.py
@@ -21,25 +21,28 @@ sushi = []
 
 for section in sections:
     category_name = section.select_one('h2 > picture > img')['alt']
-    if category_name not in ['はまっこセット','地域限定','お持ち帰り']:
+    if category_name not in ['はまっこセット', '地域限定', 'お持ち帰り']:
         items = section.select('div.men-products-item')
 
         sushi_in_category = []
         for item in items:
-            text1 = item.select_one('div.men-products-item__text') 
+            text1 = item.select_one('div.men-products-item__text')
             if text1:
                 name = ""
 
-                text1 = ''.join([text for text in text1.get_text(',').split(',') if '円' not in text])
+                text1 = ''.join([text for text in text1.get_text(
+                    ',').split(',') if '円' not in text])
 
                 text2 = item.select_one('div.men-products-item__small')
                 if text2:
-                    text2 = ''.join([text for text in text2.get_text(',').split(',') if re.match(r'（.*）',text)])
+                    text2 = ''.join([text for text in text2.get_text(
+                        ',').split(',') if re.match(r'（.*）', text)])
                     name = f"{text1}{text2}"
                 else:
                     name = text1
 
-                img_url = item.select_one('div.men-products-item__thumb > img')['data-src']
+                img_url = item.select_one(
+                    'div.men-products-item__thumb > img')['data-src']
                 sushi_in_category.append({
                     "name": name,
                     "img_url": img_url,
@@ -47,6 +50,5 @@ for section in sections:
 
         sushi.append({"category": category_name, "sushi": sushi_in_category})
 
-with open('../json/hama-sushi.json','w') as f:
+with open('../json/hama-sushi.json', 'w') as f:
     json.dump(sushi, f, ensure_ascii=False, indent=2)
-
